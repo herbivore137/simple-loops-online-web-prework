eaaa56320a50b111f18bbe473933023908b9eb65
B.A. Hanlon <herbivore137@gmail.com>
2019-04-01 19:33:25 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,6 +10,9 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  loop do 
+    puts phrase
+  end
   
 end
 
